183399b1e75a4d08ffc8da5626e8922063df3118
Sadman26 <fareless.ranger@gmail.com>
2023-02-14 01:11:41 +0600

    added google auth successfully

--- app/src/main/java/com/example/loginscreen/otp.java
+++ app/src/main/java/com/example/loginscreen/otp.java
@@ -1,14 +1,94 @@
 package com.example.loginscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import com.example.loginscreen.databinding.ActivityOtpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class otp extends AppCompatActivity {
-
+    ActivityOtpBinding binding;
+    FirebaseAuth auth;
+    String verificationId;
+    ProgressDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otp);
+        binding = ActivityOtpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Sending OTP...");
+        dialog.setCancelable(false);
+        dialog.show();
+        auth = FirebaseAuth.getInstance();
+        String phoneNumber = getIntent().getStringExtra("phoneNumber");
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(otp.this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        dialog.dismiss();
+                        verificationId = s;
+                        InputMethodManager imm = (InputMethodManager)   getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                        binding.otptext.requestFocus();
+                    }
+                })
+                .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+        binding.otpverifybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = binding.otptext.getText().toString();
+                if(code.isEmpty()){
+                    binding.otptext.setError("Enter OTP");
+                    return;
+                }
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+                auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(otp.this, welcome.class);
+                            startActivity(intent);
+                            finishAffinity();
+                        }else{
+                            Toast.makeText(otp.this, "Failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
